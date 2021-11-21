69711b0800e91355966412c404603809109d0a76
João Marcos Gonçalves <joaomgoncalvesbr@gmail.com>
2021-11-21 18:11:04 -0300

    setting up boilerplate

---
+++ src/pages/index.tsx
@@ -0,0 +1,11 @@
+import Head from 'next/head';
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Início - Ignite</title>
+      </Head>
+      <h1>Hello</h1>
+    </>
+  );
+}
