f46abed246990c737f87cd8494e2c03a9bb591df
unknown <joaomgoncalvesbr@gmail.com>
2021-12-21 09:12:12 -0300

    main in page home styled

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,22 @@
 import Head from 'next/head';
+
+import styles from './home.module.scss'
 export default function Home() {
   return (
     <>
       <Head>
         <title>Início - Ignite</title>
       </Head>
-      <h1>Hello</h1>
+      <main className={styles.contentContainer}>
+        <section className={styles.hero}>
+          <span>👍 Hey, welcome</span>
+          <h1>News about the <span>React</span> world.</h1>
+          <p> Get access to all the publications <br/> 
+          <span>For $9.90 month</span>
+          </p>
+        </section>
+      <img src="/images/avatar.svg" alt="avatar"/>
+      </main>
     </>
   );
 }
