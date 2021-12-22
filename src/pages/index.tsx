951d6adc0114682d028797673181551856e69b51
unknown <joaomgoncalvesbr@gmail.com>
2021-12-22 09:08:16 -0300

    getServerSideProps working

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,17 @@
-import Head from 'next/head';
-import { SubscribeButton } from '../components/SubscribeButton';
+import { GetServerSideProps } from "next";
+import Head from "next/head";
+import { SubscribeButton } from "../components/SubscribeButton";
+import { stripe } from "../services/stripe";
+import styles from "./home.module.scss";
 
-import styles from './home.module.scss'
-export default function Home() {
+interface HomeProps {
+  product: {
+    priceId: string;
+    amount: number;
+  };
+}
+
+export default function Home({ product }: HomeProps) {
   return (
     <>
       <Head>
@@ -11,14 +20,35 @@ export default function Home() {
       <main className={styles.contentContainer}>
         <section className={styles.hero}>
           <span>👍 Hey, welcome</span>
-          <h1>News about the <span>React</span> world.</h1>
-          <p> Get access to all the publications <br/> 
-          <span>For $9.90 month</span>
+          <h1>
+            News about the <span>React</span> world.
+          </h1>
+          <p>
+            {" "}
+            Get access to all the publications <br />
+            <span>For {product.amount} month</span>
           </p>
-          <SubscribeButton/>
+          <SubscribeButton />
         </section>
-      <img src="/images/avatar.svg" alt="avatar"/>
+        <img src="/images/avatar.svg" alt="avatar" />
       </main>
     </>
   );
 }
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  const price = await stripe.prices.retrieve("price_1K9dqxGbI6nJA7GtqRAyao44", {
+    expand: ["product"],
+  });
+
+  const product = {
+    priceId: price.id,
+    amount: price.unit_amount / 100,
+  };
+
+  return {
+    props: {
+      product,
+    },
+  };
+};
