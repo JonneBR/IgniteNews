d4c32911f53918fbb7b26a976b60d60364525cf6
unknown <joaomgoncalvesbr@gmail.com>
2021-12-31 14:20:26 -0300

    checking for existing user

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,6 +1,8 @@
-import { useSession, signIn } from "next-auth/react";
 import React from "react";
+import { api } from "../../services/api";
+import { useSession, signIn } from "next-auth/react";
 import styles from "./styles.module.scss";
+import { getStripeJs } from "../../services/stripe-js";
 
 interface SubscribeButtonProps {
   priceId: string;
@@ -13,8 +15,18 @@ interface SubscribeButtonProps {
 export function SubscribeButton({ priceId }: SubscribeButtonProps) {
   const { data: session } = useSession();
 
-  function handleSubscribe() {
+  async function handleSubscribe() {
     if (!session) return signIn("github");
+
+    try {
+      const response = await api.post("/subscribe");
+      const { sessionId } = response.data;
+      const stripe = await getStripeJs();
+
+      await stripe.redirectToCheckout({ sessionId });
+    } catch (err) {
+      alert(err.message);
+    }
   }
 
   return (
