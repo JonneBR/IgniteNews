db54d54d43f82e138f9f019316f2af1a0649f401
unknown <joaomgoncalvesbr@gmail.com>
2021-12-31 12:01:06 -0300

    setting up stripe api routes

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,3 +1,4 @@
+import { useSession, signIn } from "next-auth/react";
 import React from "react";
 import styles from "./styles.module.scss";
 
@@ -5,9 +6,23 @@ interface SubscribeButtonProps {
   priceId: string;
 }
 
+//getServerSideProps (SSR)
+//getStaticProps (SSG)
+// API routes
+
 export function SubscribeButton({ priceId }: SubscribeButtonProps) {
+  const { data: session } = useSession();
+
+  function handleSubscribe() {
+    if (!session) return signIn("github");
+  }
+
   return (
-    <button type="button" className={styles.subscribeButton}>
+    <button
+      type="button"
+      className={styles.subscribeButton}
+      onClick={handleSubscribe}
+    >
       Subscribe now
     </button>
   );
